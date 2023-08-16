f1377f653639a90a8d5c4c0f640cabd58502f243
ansdmswl0722 <ansdmswl0722@gmail.com>
2023-08-16 17:42:38 +0900

    innit

---
+++ src/main/java/com/rocket/rocket_project/RocketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.rocket_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketProjectApplication.class, args);
+    }
+
+}
