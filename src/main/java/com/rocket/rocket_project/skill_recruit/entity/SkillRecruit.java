17b480a47ead2e03870e927f63e923bb7f444064
ansdmswl0722 <ansdmswl0722@gmail.com>
2023-08-18 17:29:18 +0900

    Skill_Recruit entity create

---
+++ src/main/java/com/rocket/rocket_project/skill_recruit/entity/SkillRecruit.java
@@ -0,0 +1,33 @@
+package com.rocket.rocket_project.skill_recruit.entity;
+
+import com.rocket.rocket_project.recruit.entity.Recruit;
+import com.rocket.rocket_project.skill.entity.Skill;
+import com.rocket.rocket_project.type.entity.Type;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Skill_recruit")
+@NoArgsConstructor
+@Getter
+public class SkillRecruit {
+    @EmbeddedId
+    private Pk pk;
+
+    @Embeddable
+    @EqualsAndHashCode
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Pk implements Serializable {
+        @JoinColumn(name = "skill_seq",nullable = false)
+        @ManyToOne
+        private Skill skillSeq;
+        @JoinColumn(name = "recruit_seq",nullable = false)
+        @ManyToOne
+        private Recruit recruitSeq;
+    }
+}
