be8c97ef35134c5ea07cd7b2d25c8b1e45d9ee63
ansdmswl0722 <ansdmswl0722@gmail.com>
2023-08-22 15:17:27 +0900

    SwaggerConfiguration 설정

--- src/main/java/com/rocket/rocket_project/RocketProjectApplication.java
+++ src/main/java/com/rocket/rocket_project/RocketProjectApplication.java
@@ -2,6 +2,7 @@ package com.rocket.rocket_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class RocketProjectApplication {
