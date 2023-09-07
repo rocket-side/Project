54e9cca672e73a546560bd207daa633c073b603d
ansdmswl0722 <ansdmswl0722@gmail.com>
2023-09-07 17:51:26 +0900

    community entity 수정

--- src/main/java/com/rocket/rocket_project/community/entity/Community.java
+++ src/main/java/com/rocket/rocket_project/community/entity/Community.java
@@ -1,5 +1,6 @@
 package com.rocket.rocket_project.community.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,8 +28,28 @@ public class Community {
     private Integer likeCount;
     @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false)
+    private String title;
     @JoinColumn(name = "category_seq")
     @ManyToOne
     private Category categorySeq;
+
+    @Builder
+    public Community(Long postSeq, Long memberSeq, LocalDateTime createAt, Integer viewCount, Integer likeCount, String content,
+                     String title, Category categorySeq) {
+        this.memberSeq = memberSeq;
+        this.createAt = createAt;
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+        this.content = content;
+        this.title = title;
+        this.categorySeq = categorySeq;
+        this.postSeq = postSeq;
+    }
+
+    public void likeUpdate() {
+        this.likeCount = this.likeCount + 1;
+    }
 
 }
